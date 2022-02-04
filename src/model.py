9e07ae61f0265ea392ad6e602acdc775415a0976
Ahmed Begga <abh28@alu.ua.es>
2022-02-04 11:25:58 +0100

    Añadidos nuevos clasificadores y añadida la hungarian loss

--- src/model.py
+++ src/model.py
@@ -24,8 +24,8 @@ class FSEncoder(nn.Module):
             nn.ReLU(),
             nn.Conv1d(dim, output_channels, 1),
         )
+        
         self.pool = FSPool(output_channels, 20, relaxed=False)
-        self.salida = nn.Linear(64, 10)
 
     def forward(self, x, mask=None):
         mask = mask.unsqueeze(1)
@@ -33,6 +33,115 @@ class FSEncoder(nn.Module):
         x = self.conv(x)
         x = x / x.size(2)  # normalise so that activations aren't too high with big sets
         x, _ = self.pool(x)
-        x = self.salida(x)
-        x = F.log_softmax(x)
+        return x
+
+class FSEncoderClasification(nn.Module):
+    def __init__(self, input_channels, output_channels, dim):
+        super().__init__()
+        for m in self.modules():
+            if (
+                isinstance(m, nn.Linear)
+                or isinstance(m, nn.Conv2d)
+                or isinstance(m, nn.Conv1d)
+            ):
+                init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+        self.conv = nn.Sequential(
+            nn.Conv1d(input_channels, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(dim, dim, 1),
+        )
+        self.lin = nn.Sequential(
+            nn.Linear(dim, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim, output_channels, 1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(output_channels, output_channels),
+            nn.ReLU(),
+            nn.Linear(output_channels, 10),
+        )
+        self.pool = FSPool(dim, 20, relaxed=False)
+
+    def forward(self, x, mask=None):
+        x = self.conv(x)
+        x, perm = self.pool(x)
+        x = self.lin(x)
+        x = self.classifier(x)
+        return x
+
+class SumEncoder(nn.Module):
+    def __init__(self, *, input_channels, output_channels, dim, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(input_channels, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(dim, dim, 1),
+        )
+        self.lin = nn.Sequential(
+            nn.Linear(dim, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim, output_channels, 1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(output_channels, output_channels),
+            nn.ReLU(),
+            nn.Linear(output_channels, 10),
+        )
+
+    def forward(self, x, n_points, *args):
+        x = self.conv(x)
+        x = x.sum(2)
+        x = self.lin(x)
+        return x
+
+class MaxEncoder(nn.Module):
+    def __init__(self, *, input_channels, output_channels, dim, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(input_channels, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(dim, dim, 1),
+        )
+        self.lin = nn.Sequential(
+            nn.Linear(dim, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim, output_channels, 1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(output_channels, output_channels),
+            nn.ReLU(),
+            nn.Linear(output_channels, 10),
+        )
+
+    def forward(self, x, n_points, *args):
+        x = self.conv(x)
+        x = x.max(2)[0]
+        x = self.lin(x)
+        return x
+
+class MeanEncoder(nn.Module):
+    def __init__(self, *, input_channels, output_channels, dim, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(input_channels, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(dim, dim, 1),
+        )
+        self.lin = nn.Sequential(
+            nn.Linear(dim, dim, 1),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim, output_channels, 1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(output_channels, output_channels),
+            nn.ReLU(),
+            nn.Linear(output_channels, 10),
+        )
+
+    def forward(self, x, n_points, *args):
+        x = self.conv(x)
+        x = x.sum(2) / n_points.unsqueeze(1).float()
+        x = self.lin(x)
         return x
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,21 @@
 import torch
 import torch.nn.functional as F
+import scipy.optimize
+
+def per_sample_hungarian_loss(sample_np):
+    row_idx, col_idx = scipy.optimize.linear_sum_assignment(sample_np)
+    return row_idx, col_idx
+def hungarian_loss(predictions, targets):
+        # predictions and targets shape :: (n, c, s)
+        predictions, targets = outer(predictions, targets)
+        # squared_error shape :: (n, s, s)
+        squared_error = (predictions - targets).pow(2).mean(1)
+
+        squared_error_np = squared_error.detach().cpu().numpy()
+        indices = map(per_sample_hungarian_loss, squared_error_np)
+        losses = [sample[row_idx, col_idx].mean() for sample, (row_idx, col_idx) in zip(squared_error, indices)]
+        total_loss = torch.mean(torch.stack(list(losses)))
+        return total_loss
 
 def chamfer_loss(predictions, targets):
     # predictions and targets shape :: (k, n, c, s)
